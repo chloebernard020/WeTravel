d22f8e3e82e0e7a88d3c466b5bb1c9d82dd4e7ad
chloebernard020 <104193597+chloebernard020@users.noreply.github.com>
2023-01-31 16:39:42 +0100

    Creation DB

---
+++ Data/WeTravelContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+public class WeTravelContext : DbContext
+{
+    public DbSet<Appreciation> Appreciations { get; set; } = null!;
+    public DbSet<Compte> Comptes { get; set; } = null!;
+    public DbSet<Continent> Continents { get; set; } = null!;
+    public DbSet<Culture> Cultures { get; set; } = null!;
+    public DbSet<Lieu> Lieux { get; set; } = null!;
+    public DbSet<Pays> Pays { get; set; } = null!;
+    public DbSet<Ville> Villes { get; set; } = null!;
+    public DbSet<Visite> Visites { get; set; } = null!;
+
+    public string DbPath { get; private set; }
+
+    public WeTravelContext()
+    {
+        // Path to SQLite database file
+        DbPath = "WeTravel.db";
+    }
+
+    // The following configures EF to create a SQLite database file locally
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // Use SQLite as database
+        options.UseSqlite($"Data Source={DbPath}");
+        // Optional: log SQL queries to console
+        //options.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+    }
+}
---
+++ Migrations/20230131153852_InitialCreate.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeTravel.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comptes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Prenom = table.Column<string>(type: "TEXT", nullable: false),
+                    Mail = table.Column<string>(type: "TEXT", nullable: false),
+                    MotDePasse = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comptes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Continents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Continents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    ContinentId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pays_Continents_ContinentId",
+                        column: x => x.ContinentId,
+                        principalTable: "Continents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Villes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    PaysId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Villes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Villes_Pays_PaysId",
+                        column: x => x.PaysId,
+                        principalTable: "Pays",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lieux",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    VilleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CompteId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CompteId1 = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lieux", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lieux_Comptes_CompteId",
+                        column: x => x.CompteId,
+                        principalTable: "Comptes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Lieux_Comptes_CompteId1",
+                        column: x => x.CompteId1,
+                        principalTable: "Comptes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Lieux_Villes_VilleId",
+                        column: x => x.VilleId,
+                        principalTable: "Villes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appreciations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Commentaire = table.Column<string>(type: "TEXT", nullable: false),
+                    LieuId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CompteId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appreciations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appreciations_Comptes_CompteId",
+                        column: x => x.CompteId,
+                        principalTable: "Comptes",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Appreciations_Lieux_LieuId",
+                        column: x => x.LieuId,
+                        principalTable: "Lieux",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cultures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    LieuId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cultures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cultures_Lieux_LieuId",
+                        column: x => x.LieuId,
+                        principalTable: "Lieux",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Compte = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LieuId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Visites_Lieux_LieuId",
+                        column: x => x.LieuId,
+                        principalTable: "Lieux",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appreciations_CompteId",
+                table: "Appreciations",
+                column: "CompteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appreciations_LieuId",
+                table: "Appreciations",
+                column: "LieuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cultures_LieuId",
+                table: "Cultures",
+                column: "LieuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lieux_CompteId",
+                table: "Lieux",
+                column: "CompteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lieux_CompteId1",
+                table: "Lieux",
+                column: "CompteId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lieux_VilleId",
+                table: "Lieux",
+                column: "VilleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pays_ContinentId",
+                table: "Pays",
+                column: "ContinentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Villes_PaysId",
+                table: "Villes",
+                column: "PaysId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visites_LieuId",
+                table: "Visites",
+                column: "LieuId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appreciations");
+
+            migrationBuilder.DropTable(
+                name: "Cultures");
+
+            migrationBuilder.DropTable(
+                name: "Visites");
+
+            migrationBuilder.DropTable(
+                name: "Lieux");
+
+            migrationBuilder.DropTable(
+                name: "Comptes");
+
+            migrationBuilder.DropTable(
+                name: "Villes");
+
+            migrationBuilder.DropTable(
+                name: "Pays");
+
+            migrationBuilder.DropTable(
+                name: "Continents");
+        }
+    }
+}
